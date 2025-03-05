d3fc6b98b236a303b32a89698999b62e73348bbf
Alfredo Alas <alfredo.alas14@gmail.com>
2025-03-05 09:45:48 -0600

    add SubnetRequest.java

--- src/main/java/biz/utilsdev/subnetcalculator/dto/SubnetRequest.java
+++ src/main/java/biz/utilsdev/subnetcalculator/dto/SubnetRequest.java
@@ -1,2 +1,16 @@
-package biz.utilsdev.subnetcalculator.dto;public class SubnetRequest {
+package biz.utilsdev.subnetcalculator.dto;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SubnetRequest {
+
+    private String ipBase;
+    private int cidr;
+    private int numSubnets;
+    private List<Integer> hostRequirements;
+
 }
