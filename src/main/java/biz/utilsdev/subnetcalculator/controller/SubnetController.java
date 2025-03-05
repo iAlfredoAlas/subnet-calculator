18313bf2e8592ab7628457ff798722c4df2b302f
Alfredo Alas <alfredo.alas14@gmail.com>
2025-03-05 09:47:46 -0600

    add annotations

--- src/main/java/biz/utilsdev/subnetcalculator/controller/SubnetController.java
+++ src/main/java/biz/utilsdev/subnetcalculator/controller/SubnetController.java
@@ -6,7 +6,11 @@ import biz.utilsdev.subnetcalculator.service.SubnetCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/subnet")
 public class SubnetController {
 
     @Autowired
