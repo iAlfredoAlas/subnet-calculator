cf7f9d92ec6ab3d5c6c98713b20d94824a3bee70
Alfredo Alas <alfredo.alas14@gmail.com>
2025-03-05 08:30:04 -0600

    add project files

---
+++ src/main/java/biz/utilsdev/subnetcalculator/SubnetCalculatorApplication.java
@@ -0,0 +1,13 @@
+package biz.utilsdev.subnetcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubnetCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubnetCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/utilsdev/subnetcalculator/SubnetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.utilsdev.subnetcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubnetCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
