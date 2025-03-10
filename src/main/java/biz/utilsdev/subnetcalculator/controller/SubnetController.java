b9a00e26cddc8660220ac9c78ecee2324dda2bb0
Alfredo Alas <alfredo.alas14@gmail.com>
2025-03-09 23:40:00 -0600

    add validations in controller

--- src/main/java/biz/utilsdev/subnetcalculator/controller/SubnetController.java
+++ src/main/java/biz/utilsdev/subnetcalculator/controller/SubnetController.java
@@ -3,11 +3,18 @@ package biz.utilsdev.subnetcalculator.controller;
 import biz.utilsdev.subnetcalculator.dto.SubnetRequest;
 import biz.utilsdev.subnetcalculator.dto.SubnetResponse;
 import biz.utilsdev.subnetcalculator.service.SubnetCalculatorService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/subnet")
@@ -17,8 +24,14 @@ public class SubnetController {
     private SubnetCalculatorService subnetCalculatorService;
 
     @PostMapping("/calculate")
-    public SubnetResponse calculateSubnet(@RequestBody SubnetRequest request) {
-        return subnetCalculatorService.calculateSubnets(request);
+    public ResponseEntity<?> calculateSubnet(@Valid @RequestBody SubnetRequest request, BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+        SubnetResponse response = subnetCalculatorService.calculateSubnets(request);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
