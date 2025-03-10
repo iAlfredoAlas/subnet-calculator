eff1679dd734f859f9750b736267f6be2f6378cf
Alfredo Alas <alfredo.alas14@gmail.com>
2025-03-10 11:16:30 -0600

    add default tests

---
+++ src/test/java/biz/utilsdev/subnetcalculator/test/SubnetCalculatorApplication.java
@@ -0,0 +1,13 @@
+package biz.utilsdev.subnetcalculator.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubnetCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubnetCalculatorApplication.class, args);
+    }
+
+}
