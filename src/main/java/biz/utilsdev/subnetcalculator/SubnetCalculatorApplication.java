866932dc0b05c46c994c6adae0457ccb89b05c1e
Alfredo Alas <alfredo.alas14@gmail.com>
2025-03-10 14:25:17 -0600

    Added AGPL 3.0 license for open-source compliance

--- src/main/java/biz/utilsdev/subnetcalculator/SubnetCalculatorApplication.java
+++ src/main/java/biz/utilsdev/subnetcalculator/SubnetCalculatorApplication.java
@@ -3,6 +3,22 @@ package biz.utilsdev.subnetcalculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Nombre del proyecto: Subnet Calculator
+ * Copyright (C) 2025 Alfredo Alas
+ *
+ * Este programa es software libre: puedes redistribuirlo y/o modificarlo bajo los términos de la Licencia
+ * Pública General Affero de GNU publicada por la Free Software Foundation, ya sea la versión 3 de la Licencia,
+ * o (a tu elección) cualquier versión posterior.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA; incluso sin la garantía
+ * implícita de COMERCIABILIDAD o APTITUD PARA UN PROPÓSITO PARTICULAR. Consulta la Licencia Pública General Affero
+ * de GNU para más detalles.
+ *
+ * Deberías haber recibido una copia de la Licencia Pública General Affero de GNU junto con este programa.
+ * Si no, consulta <https://www.gnu.org/licenses/>.
+ */
+
 @SpringBootApplication
 public class SubnetCalculatorApplication {
 
